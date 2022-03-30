bdf9c0494359da4c4ec802fe325b291ad3bd07ce
Anemona Maria M <57135826+anemonamaria@users.noreply.github.com>
2022-03-30 19:06:52 +0300

    Add files via upload

--- libso.c
+++ libso.c
@@ -23,53 +23,70 @@ typedef struct _so_file {
 
 int so_fflush(SO_FILE *stream) {
 	int rc = 0, p = 0;
-	char *aux;
 
-	while (stream->cursor > 0) {
-		aux = stream->buffer + p;
-		rc = write(stream->fd, aux, stream->cursor);
-		if (rc < 0)
+	if(!stream)
+		return SO_EOF;
+
+	while ((stream->cursor -= rc) > 0) {
+		if ((rc = write(stream->fd, stream->buffer + p, stream->cursor)) < 0)
 			return SO_EOF;
-		stream->cursor -= rc;
+		// stream->cursor -= rc;
 		p += rc;
 	}
 	return 0;
+
 }
 
 int so_fseek(SO_FILE *stream, long offset, int whence) {
 	int ret = 0;
 
-	if (stream->rdwr == 2)
+	if(!stream)
+		return SO_EOF;
+
+	if (stream->rdwr == 2) {
 		so_fflush(stream);
-	ret = lseek(stream->fd, offset, whence);
-	stream->cursor = 0;
-	if (ret < 0)
+		stream->cursor = stream->cursor + lseek(stream->fd, 0, SEEK_CUR);
+	}
+	else
+		stream->cursor = BUFSIZE;
+
+	if ((ret = lseek(stream->fd, offset, whence)) < 0)
 		return -1;
+	stream->cursor = 0;
+
+
 	return 0;
 }
 
+// review
 long so_ftell(SO_FILE *stream) {
-	long ret = lseek(stream->fd, 0, SEEK_CUR);
+	long ret;
 
-	if (ret < 0) {
+	if(!stream)
+		return SO_EOF;
+
+	if ((ret = lseek(stream->fd, 0, SEEK_CUR)) < 0) {
 		stream->error = 1;
 		return SO_EOF;
 	}
+
 	if (stream->rdwr == 2)
-		ret += stream->cursor;
+		return ret + stream->cursor;
 	else if (stream->rdwr == 1)
-		ret += (stream->cursor - BUFSIZE);
-	return ret;
+		return ret + (stream->cursor - BUFSIZE);
+	return stream->cursor;
 }
 
 size_t so_fread(void *ptr, size_t size, size_t nmemb, SO_FILE *stream) {
-	int i, j;
+	int i, j, read = 0;
 	int aux;
+
+	if(!stream)
+		return SO_EOF;
 
 	for (i = 0; i < nmemb; i++)
 		for (j = 0; j < size; j++) {
-			aux = so_fgetc(stream);
-			if (aux != SO_EOF)
+			if ((aux = so_fgetc(stream)) != SO_EOF)
 				memcpy((ptr + i * size + j), &aux, 1);
 			else
 				return i;
@@ -111,6 +128,47 @@ int so_fgetc(SO_FILE *stream) {
 	stream->cursor++;
 	stream->rdwr = 1;
 	return (unsigned char) stream->buffer[stream->cursor - 1];
+}
+
+SO_FILE *so_fopen(const char *pathname, const char *mode)
+{
+	SO_FILE *f;
+
+	if(strcmp(pathname, "") == 0)
+		return NULL;
+
+	f = malloc(sizeof(SO_FILE));
+
+	if(!f)
+		return NULL;
+
+	f->fd = -1;
+	f->eof = 0;
+	f->error = 0;
+	f->cursor = 0;
+	f->charRead = 0;
+	f->rdwr = 0;
+
+	if (mode != NULL) {
+		if (strcmp(mode, "r") == 0)
+			f->fd = open(pathname, O_RDONLY);
+		if (strcmp(mode, "r+") == 0)
+			f->fd = open(pathname, O_RDWR);
+		if (strcmp(mode, "w") == 0)
+			f->fd = open(pathname, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		if (strcmp(mode, "w+") == 0)
+			f->fd = open(pathname, O_RDWR | O_CREAT | O_TRUNC, 0644);
+		if (strcmp(mode, "a") == 0)
+			f->fd = open(pathname, O_APPEND | O_WRONLY, 0644);
+		if (strcmp(mode, "a+") == 0)
+			f->fd = open(pathname, O_APPEND | O_RDWR, 0644);
+		if (f->fd < 0) {
+			free(f);
+			return NULL;
+		}
+	}
+
+	return f;
 }
 
 int so_fputc(int c, SO_FILE *stream) {
@@ -190,42 +248,20 @@ int so_pclose(SO_FILE *stream) {
 
 	if (so_fclose(stream) == SO_EOF)
 		return SO_EOF;
-	waitpid(pid, &status, 0);
-	return status;
+
+	if (waitpid(pid, &status, 0) < 0)
+		return SO_EOF;
+
+	return WEXITSTATUS(status);
 }
 
-SO_FILE *so_fopen(const char *pathname, const char *mode)
-{
-	int fd = -1;
-	SO_FILE *f;
-
-	if (strcmp(mode, "r") == 0)
-		fd = open(pathname, O_RDONLY, 0644);
-	if (strcmp(mode, "w") == 0)
-		fd = open(pathname, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (strcmp(mode, "a") == 0)
-		fd = open(pathname, O_APPEND | O_WRONLY, 0644);
-	if (strcmp(mode, "r+") == 0)
-		fd = open(pathname, O_RDWR, 0644);
-	if (strcmp(mode, "w+") == 0)
-		fd = open(pathname, O_RDWR | O_CREAT | O_TRUNC, 0644);
-	if (strcmp(mode, "a+") == 0)
-		fd = open(pathname, O_APPEND | O_RDWR, 0644);
-	if (fd < 0)
-		return NULL;
-	f = malloc(sizeof(SO_FILE));
-	f->fd = fd;
-	f->eof = 0;
-	f->error = 0;
-	f->cursor = 0;
-	f->charRead = 0;
-	f->rdwr = 0;
-	return f;
-}
-
+// review
 int so_fclose(SO_FILE *stream)
 {
 	int rc, rc2 = 0;
+
+	if (stream == NULL)
+		return SO_EOF;
 
 	if (stream->rdwr == 2)
 		rc2 = so_fflush(stream);
